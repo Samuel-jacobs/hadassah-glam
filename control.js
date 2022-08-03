54d08d2d8a7fc62ff854f893d9beadd2c7a05782
Samuel Jacobs <jacobssamuel57@gmail.com>
2022-08-03 02:14:41 +0100

    new changes

---
+++ control.js
@@ -0,0 +1,29 @@
+function openNav() {
+	let x = window.matchMedia("(max-width: 768px)");
+	function myFunction(x) {
+		if (x.matches) {
+			document.getElementById("myNav").style.height = "100%";
+			//document.body.style.backgroundColor = "green";
+		} else {
+			//document.body.style.backgroundColor = "blue";
+		}
+	}
+	myFunction(x);
+	x.addListener(myFunction);
+} 
+
+//function for opening nav
+function closeNav() {
+	let x = window.matchMedia("(max-width: 768px)");
+	function myFunction(x) {
+		if (x.matches) {
+			document.getElementById("myNav").style.height = "0%";
+			//document.body.style.backgroundColor = "green";
+		} else {
+			document.getElementById("myNav").style.height = "100%";
+			//document.body.style.backgroundColor = "blue";
+		}
+	}
+	myFunction(x);
+	x.addListener(myFunction);
+} 
