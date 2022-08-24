c0c15cd91aa6331db15b5c19c28c3ee8f7aa470f
Samuel Jacobs <jacobssamuel57@gmail.com>
2022-08-24 23:42:01 +0100

    new commits

--- control.js
+++ control.js
@@ -2,9 +2,10 @@ function openNav() {
 	let x = window.matchMedia("(max-width: 768px)");
 	function myFunction(x) {
 		if (x.matches) {
-			document.getElementById("myNav").style.height = "100%";
+			document.getElementById("myNav").style.width = "250px";
 			//document.body.style.backgroundColor = "green";
 		} else {
+			document.getElementById("myNav").style.width = "100%";
 			//document.body.style.backgroundColor = "blue";
 		}
 	}
@@ -17,10 +18,10 @@ function closeNav() {
 	let x = window.matchMedia("(max-width: 768px)");
 	function myFunction(x) {
 		if (x.matches) {
-			document.getElementById("myNav").style.height = "0%";
+			document.getElementById("myNav").style.width = "0";
 			//document.body.style.backgroundColor = "green";
 		} else {
-			document.getElementById("myNav").style.height = "100%";
+			document.getElementById("myNav").style.width = "100%";
 			//document.body.style.backgroundColor = "blue";
 		}
 	}
